118813682c6510c063cde5b09ae96d9037612720
Mateusz Zatorski <mateuszzatorski@gmail.com>
2015-07-28 08:58:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+React Redux Boilerplate
+===
+
+I found myself writing this boilerplate over and over... Might be useful.
+
+### Get started
+Check out [redux docs](https://github.com/gaearon/redux).
+
+1. `npm install && npm start`
---
+++ containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import SomeApp from './SomeApp';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from '../reducers';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        {() => <SomeApp /> }
+      </Provider>
+    );
+  }
+}
